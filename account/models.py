3490dee80a79f940c1405fab5bb9bf37a91a000c
andrew-saeed <andrew-saeed.dev@outlook.com>
2025-05-04 00:00:00 +0000

    Profile model, extract nav.html

--- account/models.py
+++ account/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.conf import settings
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, 
+        on_delete=models.CASCADE
+    )
+    date_of_birth = models.DateField(blank=True, null=True)
+    photo = models.ImageField(
+        upload_to='users/%Y/%m/%d/',
+        blank=True
+    )
+
+    def get_photo_url(self):
+        if self.photo:
+            return self.photo.url
+        return f'https://ui-avatars.com/api/?name={self.user.username}&background=d1d5dc&color=000&bold=true&font-size=0.38'
+    
+@receiver(pre_save, sender=Profile)
+def delete_old_photo(sender, instance, **kwargs):
+    if not instance.pk:
+        return False
+    
+    try:
+        old_instance = Profile.objects.get(pk=instance.pk)
+        if old_instance.photo and old_instance.photo != instance.photo:
+            old_instance.photo.delete(save=False)
+    except Profile.DoesNotExist:
+        pass
--- account/views.py
+++ account/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .forms import UserRegistrationForm
+from .models import Profile
 from django.contrib.auth.decorators import login_required
 
 def register(request):
@@ -9,6 +10,7 @@ def register(request):
             new_user = user_form.save(commit=False)
             new_user.set_password(user_form.cleaned_data['password'])
             new_user.save()
+            Profile.objects.create(user=new_user)
             return render(request, 'account.register_done.html', {'new_user':new_user})
     else:
         user_form = UserRegistrationForm()
